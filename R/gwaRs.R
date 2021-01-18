9954a18e93b2f44a75b55ab5bc1e3beab1207e54
LindoNkambule <lindonkambule116@gmail.com>
2021-01-18 11:23:30 +0200

    corrected email address

--- R/gwaRs.R
+++ R/gwaRs.R
@@ -2,7 +2,7 @@
 #'
 #' A package for creating Manhattan, Q-Q, and PCA plots for GWAS data.
 #'
-#' @author Lindokuhle Nkambule \email{lindonkambule@gmail.com}
+#' @author Lindokuhle Nkambule \email{lindonkambule116@gmail.com}
 #' @docType package
 #' @name gwaRs
 "_PACKAGE"
