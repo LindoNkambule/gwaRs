5bd92bb323e81834cc4a2d2c7c2242353b568e2d
LindoNkambule <lindonkambule116@gmail.com>
2021-04-19 22:24:09 +0200

    added zzz.R file to print message when users load the package

---
+++ R/zzz.R
@@ -0,0 +1,36 @@
+# Copyright (C) Lindokuhle Nkambule
+#
+# This file is part of gwaRs
+#
+# gwaRs is free software: you can redistribute it and/or modify it
+# under the terms of the MIT as published by
+# the Free Software Foundation.
+#
+# gwaRs is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+#
+#  A copy of the MIT is available at
+#  http://www.r-project.org/Licenses/
+#
+
+
+#' @import utils
+NULL
+
+.onAttach <- function(lib, pkg,...){
+  packageStartupMessage(gwaRsWelcomeMessage())
+}
+
+
+gwaRsWelcomeMessage <- function(){
+
+  cat("Thank you for using gwaRs (version ",utils::packageDescription("ggplot2")$Version, ")\n",
+      "To access the overall documentation, go to: https://cran.r-project.org/web/packages/gwaRs/vignettes/gwaRs.html\n",
+      "Suggestions and bug-reports can be submitted at: https://github.com/LindoNkambule/gwaRs/issues\n",
+      "\n",
+      "\tTo suppress this message use:\n",
+      "\tsuppressPackageStartupMessages(library(gwaRs))\n",
+      sep="")
+}
